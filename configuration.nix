a6f612dd960601f9b52b1ba6cb393c66b99d526b
Octaeon <octaeonspan@gmail.com>
2025-03-03 18:16:40 +0100

    It turns out you need wayland to run hyprland

--- configuration.nix
+++ configuration.nix
@@ -71,6 +71,8 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     # vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    kdePackages.wayland
+    gdm
     wget
     git
     curl
