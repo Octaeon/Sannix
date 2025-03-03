203d8887a8a4039174ad1f95341f6c828be31b5f
Octaeon <octaeonspan@gmail.com>
2025-03-03 17:42:21 +0100

    Added default fonts and lazygit

--- home.nix
+++ home.nix
@@ -4,15 +4,26 @@
   home.username = "main";
   home.homeDirectory = "/home/main";
 
+  home.packages = with pkgs; [
+    lazygit
+  ];
+
   programs.git = {
     enable = true;
-	userName = "Octaeon";
-	userEmail = "octaeonspan@gmail.com";
+	  userName = "Octaeon";
+	  userEmail = "octaeonspan@gmail.com";
   };
 
   programs.starship = {
     enable = true;
   };
 
+  fonts.fontconfig = {
+    enable = true;
+    defaultFonts = {
+      monospace = [ "JetBrainsMono" ];
+    };
+  };
+  
   home.stateVersion = "24.11";
 }
